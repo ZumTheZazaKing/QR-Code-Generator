820852c3846c384f8a40c7d67aee091f55d5fa92
ZumTheZazaKing <zahidi85543@gmail.com>
2021-09-14 20:24:01 +0800

    Added download button

--- src/Output.js
+++ src/Output.js
@@ -1,6 +1,7 @@
 import { Context } from './Contexts/Context';
 
 import { useContext } from 'react';
+import Button from '@material-ui/core/Button';
 
 export function Output(){
 
@@ -8,7 +9,11 @@ export function Output(){
 
     return (<div id="Output">
         {qrImage ? 
-        <img src={qrImage} alt="" title="" />
+        <div id="main">
+            <img src={qrImage} alt="" title="" />
+            <br/>
+            <Button>DOWNLOAD SVG</Button>
+        </div>
         :
         <h2>Start by entering a link or text!</h2>}
     </div>)
